41bc9d8ae023a1973a6d346ebf8aa9e488dd1775
Gman <germangast@gmail.com>
2021-01-08 23:11:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFindingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid grid = new Grid(100, 40);
+            Console.WriteLine(grid.GetRepresented());
+            Console.ReadKey();
+        }
+    }
+}
