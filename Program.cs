7c6be9a9936032d016e10b0320162dab3ef841cf
Germán Gasset Martí <76014247+GGasset@users.noreply.github.com>
2021-01-09 15:41:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFindingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid grid = new Grid(20,20);
+            grid.GetCell(grid.size.Width / 2-1, grid.size.Height / 2-1).CellState = Cell.CellStates.Wall;
+            Console.WriteLine(grid.GetRepresented());
+            Console.ReadKey();
+        }
+    }
+}
